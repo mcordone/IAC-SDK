50893ca6ebd209d461fbf3ddca9b976766fd439a
mcordones <miguelcordones@northwesternmutual.com>
2019-03-31 16:56:13 -0400

    adding event producer biz logic

--- src/main/java/com/iac/sdk/IacSdkApplication.java
+++ src/main/java/com/iac/sdk/IacSdkApplication.java
@@ -1,9 +1,45 @@
 package com.iac.sdk;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iac.sdk.domain.Event;
+import com.iac.sdk.producer.EventProducer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class IacSdkApplication
 {
     public static void main( String[] args )
     {
+        BlockingQueue<Event> queue = new LinkedBlockingQueue<>();
+        List<Event> eventData = loadEventData();
 
+        new Thread(new EventProducer(queue, eventData)).start();
+    }
+
+    /**
+     *
+     * @return list of events
+     */
+    private static List<Event> loadEventData() {
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        File jsonFile = new File(classLoader.getResource("eventData.json").getFile());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Event> eventData = new ArrayList<>();
+
+        try {
+            eventData = objectMapper.readValue(jsonFile, new TypeReference<List<Event>>(){});
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return eventData;
     }
 }
---
+++ src/main/java/com/iac/sdk/producer/EventProducer.java
@@ -0,0 +1,46 @@
+package com.iac.sdk.producer;
+
+import com.iac.sdk.domain.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
+public class EventProducer implements Runnable {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(EventProducer.class);
+    private static long SLEEP_TIME_INTERVAL = 1000;
+
+    private BlockingQueue<Event> queue;
+    private List<Event> eventData;
+
+    public EventProducer(BlockingQueue<Event> queue, List<Event> eventData) {
+        this.queue = queue;
+        this.eventData = eventData;
+    }
+
+    public void run() {
+        try{
+            while (true) {
+                generateEvents();
+            }
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * Utility function to generate Events objects for this test application. Generate eventList size per seconds
+     * In real application the mobile device generates these events
+     * @throws InterruptedException
+     */
+    private void generateEvents() throws InterruptedException {
+        for (int i = 0; i < eventData.size(); i++) {
+            queue.put(eventData.get(i));
+        }
+
+        Thread.sleep(SLEEP_TIME_INTERVAL);
+    }
+}
